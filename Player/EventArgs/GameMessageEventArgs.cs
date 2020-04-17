0dc13d2aa141a1aa62188ceb423b2390e0f1edd6
AshtrayShetty <51413934+AshtrayShetty@users.noreply.github.com>
2020-04-17 20:08:46 +0530

    Event handlers for in-game events

---
+++ Player/EventArgs/GameMessageEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.EventArgs
+{
+    public class GameMessageEventArgs : System.EventArgs
+    {
+        public string message { get; set; }
+        public GameMessageEventArgs(string message) { this.message = message; }
+    }
+}
